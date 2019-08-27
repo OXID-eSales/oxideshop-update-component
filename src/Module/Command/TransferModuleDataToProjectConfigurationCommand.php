c7d5c5b60e46e2e1db37913bec6053a2dd92c21c
iegupov <igor.iegupov@gmail.com>
2019-08-27 14:08:05 +0200

    OXDEV-2131 Introduce TransferModuleDataToProjectConfigurationCommand

---
+++ src/Module/Command/TransferModuleDataToProjectConfigurationCommand.php
@@ -0,0 +1,61 @@
+<?php declare(strict_types=1);
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidEsales\OxidEshopUpdateComponent\Module\Command;
+
+use OxidEsales\OxidEshopUpdateComponent\Module\Service\ActiveModuleStateTransferringServiceInterface;
+use OxidEsales\OxidEshopUpdateComponent\Module\Service\ModuleExtensionsSortingService;
+use OxidEsales\OxidEshopUpdateComponent\Module\Service\ModuleSettingsTransferringServiceInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @internal
+ */
+final class TransferModuleDataToProjectConfigurationCommand extends Command
+{
+    /**
+     * @var ActiveModuleStateTransferringServiceInterface
+     */
+    private $activeModuleStateTransferringService;
+
+    /**
+     * @var ModuleSettingsTransferringServiceInterface
+     */
+    private $moduleSettingsTransferringService;
+
+    /**
+     * @var ModuleExtensionsSortingService
+     */
+    private $moduleExtensionsSortingService;
+
+    public function __construct(
+        ActiveModuleStateTransferringServiceInterface $activeModuleStateTransferringService,
+        ModuleSettingsTransferringServiceInterface $moduleSettingsTransferringService,
+        ModuleExtensionsSortingService $moduleExtensionsSortingService
+    ) {
+        $this->activeModuleStateTransferringService = $activeModuleStateTransferringService;
+        $this->moduleSettingsTransferringService = $moduleSettingsTransferringService;
+        $this->moduleExtensionsSortingService = $moduleExtensionsSortingService;
+
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->setName('oe:oxideshop-update-component:transfer-module-data' );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): void
+    {
+        $this->activeModuleStateTransferringService->transferAlreadyActiveModuleStateToProjectConfiguration();
+        $output->writeln('');
+        $this->moduleSettingsTransferringService->transferValuesFromDatabaseToProjectConfiguration();
+        $output->writeln('');
+        $this->moduleExtensionsSortingService->sort();
+    }
+}
